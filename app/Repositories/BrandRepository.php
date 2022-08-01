9011333d03860006e5dcac4b72b17017f2be8777
v3ronez <v3ronez.dev@gmail.com>
2022-08-01 02:33:56 -0300

    Repository Design Pattern

--- app/Repositories/BrandRepository.php
+++ app/Repositories/BrandRepository.php
@@ -1,35 +1,7 @@
 <?php
 namespace App\Repositories;
 
-use Illuminate\Database\Eloquent\Model;
-
-class BrandRepository
+class BrandRepository extends AbstractRepository
 {
 
-    public function __construct(Model $brand)
-    {
-        $this->brand = $brand;
-    }
-
-    public function selectAttributes($attributes): void
-    {
-        $this->brand = $this->brand->with($attributes);
-    }
-
-    public function filter($filter): void
-    {
-        $filter = explode(';', $filter);
-        foreach ($filter as $key => $value) {
-            $condition = explode(':', $value);
-            $this->brand = $this->brand->where($condition[0], $condition[1], $condition[2]);
-        }
-    }
-    public function selectParams($params): void
-    {
-        $this->brand = $this->brand->selectRaw($params);
-    }
-    public function getAttribute()
-    {
-        return $this->brand->get();
-    }
 }
---
+++ app/Repositories/CarModelRepository.php
@@ -0,0 +1,7 @@
+<?php
+namespace App\Repositories;
+
+class CarModelRepository extends AbstractRepository
+{
+
+}
